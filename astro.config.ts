40a1910e08f5a4b907ced18cace1de9e6d228b08
23evanm <23evanm@students.spa.edu>
2023-01-25 21:06:09 -0600

    announcements, no styling yet

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import UnoCSS from "unocss/astro";
 import node from '@astrojs/node';
 
-
 export default defineConfig({
   integrations: [
     UnoCSS()
--- src/utils/google.ts
+++ src/utils/google.ts
@@ -2,6 +2,8 @@ import { GoogleSpreadsheet, GoogleSpreadsheetWorksheet } from "google-spreadshee
 import { SOURCES_ID } from "./constants";
 import { JWT } from "google-auth-library"
 import { drive } from "@googleapis/drive"
+import type { Dayjs } from "dayjs";
+import dayjs from "dayjs";
 
 const email = process.env.GOOGLE_SERVICE_EMAIL as string;
 const key = (process.env.GOOGLE_SERVICE_PRIVATE_KEY as string).replaceAll("\\n", "\n");
@@ -35,4 +37,39 @@ export async function getImages(): Promise<string[]> {
   })).data.files;
 
   return files?.map(file => file.webContentLink as string) ?? [];
+}
+
+const announcementsSheetId = await getSource("announcements-id");
+const announcementsSheet = new GoogleSpreadsheet(announcementsSheetId);
+await announcementsSheet.useServiceAccountAuth({
+  client_email: email,
+  private_key: key        
+});
+await announcementsSheet.loadInfo();
+
+const announcements: GoogleSpreadsheetWorksheet = announcementsSheet.sheetsByTitle[announcementsSheet.title];
+
+type Announcement = {
+  startDate: Dayjs,
+  endDate: Dayjs,
+  title: string,
+  body: string,
+  image: any
+}
+
+export async function getAnnouncements(): Promise<Announcement[]> {
+  const rows = await announcements.getRows()
+
+  const announcement = rows.map(row => {
+    return {
+      startDate: dayjs(row.start_date).subtract(1, 'day'),
+      endDate: dayjs(row.end_date).add(1, 'day'),
+      title: row.title,
+      body: row.body,
+      // ex. https://drive.google.com/open?id=IIIDDDD
+      image: row.image.replace("/open?id=", "/uc?id=")
+    }
+  })
+    .filter(a => a.startDate.isBefore(dayjs()) && a.endDate.isAfter(dayjs()))
+  return announcement;
 }
