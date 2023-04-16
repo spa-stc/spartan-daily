d3a7964fb7abd193a95c8c86c23ea9c228466a57
23evanm <23evanm@students.spa.edu>
2023-04-16 11:33:01 -0500

    updade depends

--- astro.config.ts
+++ astro.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 import deno from "@astrojs/deno";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS()],
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+    }),
+  ],
   output: "server",
   adapter: deno(),
 });
